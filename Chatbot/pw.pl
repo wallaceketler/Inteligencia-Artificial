e87bf6286c0e728688f22132262debc2854473d5
Wallace Ketler <56899474+wallaceketler@users.noreply.github.com>
2022-06-20 22:47:04 -0300

    Criação Chatbot

---
+++ Chatbot/pw.pl
@@ -0,0 +1,34 @@
+pw:-
+	%Altura0
+    format("Bem vindo(a) À PW cosméticos e perfumaria"),nl,
+	%Altura1
+    format("Deseja comprar um produto?"),nl,
+    read(resposta),
+	format("Deseja devolver um produto?"),nl,
+    read(resposta),
+    format("Deseja trocar um produto?"),nl,
+    read(resposta),
+    format("Deseja ver o status  de entrega de um produto"),nl,
+    read(resposta),
+    format("Deseja conversar um atendente?"),nl,
+    read(resposta),
+    format("Deseja saber onde temos lojas físicas?"),nl,
+    read(resposta),
+    %Altura2
+    format("Aqui está a lista de produtos disponíveis, digite o número do produto que você deseja"),nl,
+	format("Aqui está a lista de produtos que você já comprou, digite o número que deseja"),nl,
+	format("Aqui está a lista de produtos que você já comprou, digite o número que deseja"),nl,
+    format("Aqui está o status da entrega de seus produtos"),nl,
+    format("Aqui está o contato de um dos nossos atendentes"),nl,
+    format("Aqui está o endereço das lojas físicas"),nl,
+    %Altura3
+    format("Deseja concluir a compra?"),nl,
+    read(resposta),
+    format("Produto devolvido"),nl,  
+    format("Aqui está a lsita de produtos disponíveis para troca, Digite o número que deseja"),nl,
+    format("Obrigado pela compra"),nl,
+    format("Deseja concluir a troca?"),nl,
+    read(resposta),
+    %Altura4
+    format("Obrigado pela troca"),nl.
+	
